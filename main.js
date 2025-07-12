596ba0631b104952f1b468bb3101b91c2d986c1d
safooahmed1 <safoo468@gmail.com>
2025-07-12 04:28:30 +0300

    fatoraSystm-2

---
+++ main.js
@@ -0,0 +1,52 @@
+//fatora
+const fatora = [
+  // { name: "iphone x", price: "450", qty: "1" },
+  // { name: "iphone 15", price: "500", qty: "2" },
+  // { name: "iphone 16", price: "550", qty: "1" },
+];
+//Read
+let fatoraList = document.querySelector("#fatoraList");
+
+const showFatora = () => {
+  fatoraList.innerHTML = "";
+  fatora.forEach((el, index) => {
+    fatoraList.innerHTML += `
+<tr class="align-items-center">
+            <td>${index + 1}</td>
+            <td>${el.name}</td>
+            <td>${el.qty}</td>
+            <td class="text-white">
+              <button class="btn btn-primary"><i class="bi bi-plus"></i></button>
+              <button class="btn btn-danger"><i class="bi bi-dash"></i></button>
+              <button class="btn btn-danger ms-2" onclick="removFromFatora(${index})"><i class="bi bi-trash-fill"></i></button>
+            </td>
+            <td class="price">${el.price}$</td>
+            <td class="price">${el.price * el.qty}$</td>
+          </tr>
+    `;
+  });
+};
+//calc total
+const calcTotle = () => {
+  let totle = 0;
+  fatora.forEach((el) => {
+    totle += el.price * el.qty;
+  });
+  document.querySelector("#total").innerHTML = `${totle}$`;
+};
+
+//Creat
+const addToFatora = (prodactIndex) => {
+  let prodactObj = prodacts[prodactIndex];
+  delete prodactObj.stockQty;
+  delete prodactObj.img;
+  prodactObj.qty = 1;
+  fatora.push(prodactObj);
+  showFatora();
+};
+
+//delete
+const removFromFatora = (deleteIndexFatore) => {
+  fatora.splice(deleteIndexFatore, 1);
+  showFatora();
+};
---
+++ prodacts.js
@@ -0,0 +1,29 @@
+const prodacts = [
+  { name: "iphone xr", price: "450", stockQty: "5", img: "./1.jpg" },
+  { name: "iphone 15", price: "500", stockQty: "8", img: "./2.jpg" },
+  { name: "iphone 16", price: "550", stockQty: "4", img: "./3.jpg" },
+  { name: "iphone 11", price: "300", stockQty: "3", img: "./4.jpg" },
+];
+
+let productList = document.querySelector("#product-list");
+
+const showProdacts = () => {
+  prodacts.innerHTML = "";
+  prodacts.forEach((el, index) => {
+    productList.innerHTML += `
+    <div class="bg-white p-3 rounded shadow border d-flex col-4">
+      <div class="col-3 d-flex">
+        <img class="col-12 rounded" src="${el.img}" alt="${el.name}" />
+      </div>
+      <div class="col-9 d-flex flex-column">
+      <div class="ms-4">
+        <h1>${el.name}</h1>
+          <p class="price">${el.price}$</p>
+          <button onclick="addToFatora(${index})" class="btn btn-primary">Add To Fatora</button>
+        </div>
+      </div>
+    </div>
+`;
+  });
+};
+showProdacts();
